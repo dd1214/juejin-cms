3b94a9728bfa0b7226380998d7b6dc662e32fa35
liangzhuren <2689643069@qq.com>
2023-01-19 16:56:19 +0800

    提交语

--- src/pages/Welcome.tsx
+++ src/pages/Welcome.tsx
@@ -1,10 +1,9 @@
 import { PageContainer } from '@ant-design/pro-components';
 import { useModel } from '@umijs/max';
-import { Card, theme } from 'antd';
+import { Card } from 'antd';
 import React from 'react';
 
 const Welcome: React.FC = () => {
-  const { token } = theme.useToken();
   const { initialState } = useModel('@@initialState');
   return (
     <PageContainer>
@@ -27,36 +26,7 @@ const Welcome: React.FC = () => {
             backgroundImage:
               "url('https://gw.alipayobjects.com/mdn/rms_a9745b/afts/img/A*BuFmQqsB2iAAAAAAAAAAAAAAARQnAQ')",
           }}
-        >
-          <div
-            style={{
-              fontSize: '20px',
-              color: token.colorTextHeading,
-            }}
-          >
-            欢迎使用 Ant Design Pro
-          </div>
-          <p
-            style={{
-              fontSize: '14px',
-              color: token.colorTextSecondary,
-              lineHeight: '22px',
-              marginTop: 16,
-              marginBottom: 32,
-              width: '65%',
-            }}
-          >
-            Ant Design Pro 是一个整合了 umi，Ant Design 和 ProComponents
-            的脚手架方案。致力于在设计规范和基础组件的基础上，继续向上构建，提炼出典型模板/业务组件/配套设计资源，进一步提升企业级中后台产品设计研发过程中的『用户』和『设计者』的体验。
-          </p>
-          <div
-            style={{
-              display: 'flex',
-              flexWrap: 'wrap',
-              gap: 16,
-            }}
-          ></div>
-        </div>
+        ></div>
       </Card>
     </PageContainer>
   );
