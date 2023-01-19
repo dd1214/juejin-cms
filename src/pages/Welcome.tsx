38f0672061ac09078d187b3db4d8cde32c3648f5
蔺志庆 <2953504224@qq.com>
2023-01-19 20:53:15 +0800

    新增markdown编辑器

--- src/pages/Welcome.tsx
+++ src/pages/Welcome.tsx
@@ -1,8 +1,18 @@
 import { PageContainer } from '@ant-design/pro-components';
 import { useModel } from '@umijs/max';
-import { Card } from 'antd';
+import { Button, Card, Col, Form, Input, Row } from 'antd';
+import MarkdownIt from 'markdown-it';
 import React from 'react';
+import MdEditor from 'react-markdown-editor-lite';
+// import style manually
+import { CloudUploadOutlined, SendOutlined } from '@ant-design/icons';
+import 'react-markdown-editor-lite/lib/index.css';
+const mdParser = new MarkdownIt(/* Markdown-it options */);
 
+// Finish!
+function handleEditorChange({ html, text }: { html: string; text: string }) {
+  console.log('handleEditorChange', html, text);
+}
 const Welcome: React.FC = () => {
   const { initialState } = useModel('@@initialState');
   return (
@@ -23,10 +33,45 @@ const Welcome: React.FC = () => {
             backgroundPosition: '100% -30%',
             backgroundRepeat: 'no-repeat',
             backgroundSize: '274px auto',
-            backgroundImage:
-              "url('https://gw.alipayobjects.com/mdn/rms_a9745b/afts/img/A*BuFmQqsB2iAAAAAAAAAAAAAAARQnAQ')",
           }}
-        ></div>
+        >
+          {' '}
+          <Form name="basic" initialValues={{ remember: true }} autoComplete="off">
+            <Row>
+              <Col span={17}>
+                <Form.Item
+                  label="标题"
+                  name="title"
+                  rules={[{ required: true, message: '请输入标题' }]}
+                >
+                  <Input />
+                </Form.Item>
+              </Col>
+              <Col span={3} style={{ marginLeft: '20px' }}>
+                <Form.Item>
+                  <Button htmlType="submit">
+                    <CloudUploadOutlined />
+                    草稿箱
+                  </Button>
+                </Form.Item>
+              </Col>
+              <Col span={3}>
+                <Form.Item>
+                  <Button type="primary" htmlType="submit">
+                    <SendOutlined />
+                    发布
+                  </Button>
+                </Form.Item>
+              </Col>
+            </Row>
+          </Form>
+        </div>
+        <MdEditor
+          style={{ height: '500px' }}
+          renderHTML={(text) => mdParser.render(text)}
+          onChange={handleEditorChange}
+        />
+        );
       </Card>
     </PageContainer>
   );
